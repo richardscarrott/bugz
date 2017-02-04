360a5e14ea8cfef11f1df1115bf12d5cdbd6d81f
Richard Scarrott <riscarrott@googlemail.com>
2017-02-04 10:58:00 +0000

    Fix up trimChar

--- src/internal/sanitize.js
+++ src/internal/sanitize.js
@@ -6,7 +6,7 @@ const lastChar = str => str.charAt(str.length - 1);
 
 const firstCharAsNumber = compose(Number, firstChar);
 
-// Maybe safer to avoid recursive calls?
+// NOTE: Maybe safer to avoid recursive calls?
 const stripLeadingChars = val =>
   isNaN(firstCharAsNumber(val)) ? stripLeadingChars(drop(1, val)) : val;
 
@@ -14,11 +14,12 @@ const indexOfFirstNonNumeric = compose(prop('index'), match(/[^0-9\.]/));
 
 const stripTrailingChars = val => take(indexOfFirstNonNumeric(val), val);
 
+// NOTE: Maybe safer to avoid recursive calls?
 const trimChar = curry((char, val) => {
-  while (firstChar(val) === char) {
+  if (firstChar(val) === char) {
     return trimChar(char, drop(1, val));
   }
-  while (lastChar(val) === char) {
+  if (lastChar(val) === char) {
     return trimChar(char, take(val.length - 1, val));
   }
   return val;
