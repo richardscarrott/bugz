e7cc62ef72655906ae1cb03dedb2a7616f638bc8
Richard Scarrott <riscarrott@googlemail.com>
2017-02-13 23:43:49 +0000

    100% coverage

--- src/about.js
+++ src/about.js
@@ -1,4 +1,5 @@
 import { compose } from 'ramda';
 import parse from './parse';
+import console from './internal/console';
 
 export default compose('table' in console ? console.table : console.log, parse);
